89eea3e1d6d7c12a7999d6679c27213baf6e5f98
Jesus Cardenas <jesscrdng@gmail.com>
2020-03-02 14:03:36 -0500

    fundamentos

---
+++ fundamentos/flecha.js
@@ -0,0 +1,22 @@
+// function sumar(a, b) {
+//   return a + b;
+// }
+
+// let sumar = (a, b) => a + b;
+
+// function saludar() {
+//   return 'Hola Mundo';
+// }
+
+// let saludar = () => 'Hola Mundo';
+
+
+// function saludar(nombre) {
+//   return `Hola ${ nombre }`;
+// }
+
+let saludar = nombre => `Hola ${ nombre }`;
+
+console.log(saludar('Jesu'));
+
+// console.log(sumar(10, 20));
